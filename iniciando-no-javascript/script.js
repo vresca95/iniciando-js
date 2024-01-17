e8428c87436bb396a1d001da14ff1458a6068839
vresca95 <133811228+vresca95@users.noreply.github.com>
2024-01-17 15:59:45 -0300

    Add files via upload

---
+++ iniciando-no-javascript/script.js
@@ -0,0 +1,64 @@
+alert('Hello World!')
+
+const numberOne = 20
+const numberTwo = 17
+
+const sum = numberOne + numberTwo
+
+alert(sum)
+
+
+const isNumber = 13
+
+if (typeof isNumber == 'number') {
+  alert('É um número')
+} else {
+  alert('Não é um número')
+}
+
+const isString = 'Isso é uma string'
+
+if (typeof isString == 'string') {
+  alert('É uma string')
+} else {
+  alert('Não é uma string')
+}
+
+const isBoolean = true
+
+if (typeof isBoolean == 'boolean') {
+  alert('É um booleano')
+} else {
+  alert('Não é um booleano')
+}
+
+const sub = numberOne - numberTwo
+
+alert(sub)
+
+
+const mult = numberOne * numberTwo
+
+alert(mult)
+
+
+const div = numberOne / numberTwo
+
+alert(div)
+
+
+const isEvenNumber = 5
+
+if (isEvenNumber % 2 == 0) {
+  alert('É um número par')
+} else {
+  alert('Não é um número par')
+}
+
+const isOddNumber = 6
+
+if (isOddNumber % 2 != 0) {
+  alert('É um número ímpar')
+} else {
+  alert('Não é um número ímpar')
+}
